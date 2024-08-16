6b5bbd531a5425b95efa17326be982d5e569fde7
althealouise <87533603+althealouise@users.noreply.github.com>
2024-08-16 20:41:51 +0800

    added animation to theme toggle button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Sun, Moon } from 'lucide-react';
 import { useTheme } from '../ThemeContext';
 
-const ThemeToggle: React.FC = () => {
+const ThemeToggle = (): React.ReactElement => {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
@@ -13,12 +13,19 @@ const ThemeToggle: React.FC = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="fixed bottom-4 right-4"
+      className={`fixed bottom-4 right-4 p-2 rounded-full transition-colors duration-300 ease-in-out ${theme === 'light' ? 'bg-gray-800 text-blue-600' : 'bg-gray-200 text-blue-600'}`}
+      aria-label="Toggle Theme"
     >
       {theme === 'light' ? (
-        <Moon size={40} className='fixed bottom-4 right-4 rounded-full shadow-lg bg-gray-800 text-blue-600 p-2 focus:outline-none'/>
+        <Moon
+          size={20}
+          className={`transition-transform duration-500 ${theme === 'light' ? 'rotate-clockwise' : 'rotate-counterclockwise'}`}
+        />
       ) : (
-        <Sun size={40} className='fixed bottom-4 right-4 rounded-full shadow-lg bg-white text-blue-600 p-2 focus:outline-none'/>
+        <Sun
+          size={20}
+          className={`transition-transform duration-500 ${theme === 'dark' ? 'rotate-counterclockwise' : 'rotate-clockwise'}`}
+        />
       )}
     </button>
   );
